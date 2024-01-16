575ea186be829be9525267a25f4915efaea9ff47
st1020 <stone_1020@qq.com>
2024-01-16 19:15:32 +0800

    fix: float display error

--- src/utils/float.rs
+++ src/utils/float.rs
@@ -72,13 +72,13 @@ impl Hash for Float {
 
 impl fmt::Debug for Float {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Debug::fmt(&self, f)
+        fmt::Debug::fmt(&self.0, f)
     }
 }
 
 impl fmt::Display for Float {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        fmt::Display::fmt(&self, f)
+        fmt::Display::fmt(&self.0, f)
     }
 }
 
